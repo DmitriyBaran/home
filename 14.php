6197961142683d759cb32dc0249df6d7ec989510
DmitriyBaran <33020093+DmitriyBaran@users.noreply.github.com>
2017-12-05 14:09:11 +0200

    Update 14.php

--- 14.php
+++ 14.php
@@ -4,7 +4,7 @@ $arr = array(4, 2, 5, 19, 13, 0, 10);
 
 foreach ($arr as $e)
 {
-    if ($e = 2 or $e = 3 or $e = 4)
+    if ($e == 2 || $e == 3 || $e == 4)
         echo 'Есть';
     else
         echo 'Нет!';
