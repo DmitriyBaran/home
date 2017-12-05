b24d5ceefe4b8ab3b7865c012ac9b1e99afbb06b
DmitriyBaran <33020093+DmitriyBaran@users.noreply.github.com>
2017-12-05 14:14:32 +0200

    Update 15.php

--- 15.php
+++ 15.php
@@ -1,10 +1,9 @@
 <?php
 $arr = array(4, 2, 5, 19, 13, 0, 10);
-$c = 0;
-foreach ($arr as $k => $v)
+$result=0;
+foreach ($arr as $v)
 {
-
+  $result+=$v;
 }
-print_r (count($arr));
-
+echo $result;
 ?>
